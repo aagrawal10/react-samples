cda2ad03e5195deb4cb7fe9df074ff1f10d0c589
Akash Agrawal <akash@coefficient.io>
2021-06-13 18:57:44 +0530

    Adding cart-icon, cart-dropdown and cart-reducer

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component"
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
@@ -10,7 +13,7 @@ const onSignout = () => {
     console.log("Signing Out");
 }
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -30,12 +33,15 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null : (<CartDropdown />) }
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden,
+});
 
 export default connect(mapStateToProps)(Header);
